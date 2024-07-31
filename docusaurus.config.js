3200393c0de5edd12b92b96fe069f5b13aa0960b
Christian Rémillard <christian.remillard@gmail.com>
2024-07-31 10:36:54 -0400

    wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,20 +8,20 @@ import { themes as prismThemes } from 'prism-react-renderer'
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Lock 21 3D',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://lock21.info',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'remillc', // Usually your GitHub org/user name.
+  projectName: 'lock21.info', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -33,6 +33,15 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+  future: {
+    experimental_storage: {
+      type: 'localStorage',
+    }
+  },
+
+  plugins: [
+    'docusaurus-plugin-image-zoom'
+  ],
 
   presets: [
     [
@@ -45,14 +54,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/remillc/lock21.info/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/remillc/lock21.info/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -67,17 +76,17 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Lock21.info',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Lock21.info',
           src: 'img/logo.svg',
         },
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'makingOfSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Making of',
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
@@ -136,6 +145,12 @@ const config = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      zoom: {
+        // selector: '.markdown :not(em) > img',
+        // options: {
+        //   background: 'rgba(0, 0, 0, 0.5)',
+        // },
+      }
     }),
 }
 
